6793ea4c209066a8082235be16066057e6cef018
Abdul Latif <latif901@hotmail.com>
2023-05-05 20:15:04 +0400

    Probably the final commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,46 @@
 window.addEventListener("DOMContentLoaded", ()=>{
-  const video = document.getElementById("man-and-the-mountain-video")
-  video.load()
+  const videoOne = document.getElementById("man-and-the-mountain-video")
+  videoOne.load()
 
   gsap.to(".man-and-the-mountain", {
     scrollTrigger:{
       trigger: ".man-and-the-mountain",
       start: "top 70%",
       end: "bottom top",
-      onEnter: () => video.play(),
+      onEnter: () => videoOne.play(),
       onLeave: () => {
         if(innerWidth > "500" ){
-          video.load()
+          videoOne.load()
+
         }},
-      onEnterBack: () => video.play()
+      onEnterBack: () => videoOne.play()
+  
+    }
+  })
+  const videoTwo = document.getElementById("man-cooking-video")
+
+  gsap.to(".man-cooking-his-ideas", {
+    scrollTrigger:{
+      trigger: ".man-cooking-his-ideas",
+      start: "top 10%",
+      end: "bottom 80%",
+      onEnter: () => videoTwo.play(),
+      onLeave: () => {
+        if(innerWidth > "500" ){
+          videoTwo.pause()
+        }},
+      onEnterBack: () => videoTwo.play()
+  
+    }
+  })
+
+  const videoThree = document.getElementById("cool-cat")
+
+  gsap.to(".contact-section", {
+    scrollTrigger:{
+      trigger: ".contact-section",
+      start: "top 10%",
+      onEnter: () => videoThree.play()
   
     }
   })
